2b2cc5eae09ba10905222aa0d74a16d184de682a
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-01-14 23:23:07 -0300

    Add first test

---
+++ src/theory/forEach.test.ts
@@ -0,0 +1,13 @@
+import { forEach } from "./forEach"
+
+test("forEach log the array", () => {
+  const consoleLogSpy = jest.spyOn(console, "log").mockImplementation(() => {})
+
+  forEach([1, 2, 3, 4])
+
+  expect(consoleLogSpy).toHaveBeenCalledTimes(4)
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(1, 1)
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(2, 2)
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(3, 3)
+  expect(consoleLogSpy).toHaveBeenNthCalledWith(4, 4)
+})
