00e7f4d7f19f2cffcfd275d33ec1a299ec7b0bce
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-02-18 23:43:39 -0300

    Remove comment

--- src/leetCode/summedNumbers.ts
+++ src/leetCode/summedNumbers.ts
@@ -4,7 +4,6 @@
 export const summedNumbers = (array: number[], sum: number) => {
   const seen = new Set<number>()
 
-  // I used for of here because foreach doesn't stop when return
   for (const element of array) {
     const neededValue = sum - element
 
