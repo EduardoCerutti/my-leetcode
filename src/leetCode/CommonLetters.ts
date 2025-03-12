4ad6b2e4f79b7e07958ae430ff0dedd17d93b32b
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-03-11 22:41:00 -0300

    Add space complexity

--- src/leetCode/CommonLetters.ts
+++ src/leetCode/CommonLetters.ts
@@ -1,6 +1,7 @@
 // check if both arrays has at least one letter in common
 // [g, a, w, j] [b, q, j] should return true
 
+// Space complexity: O(n)
 // Time complexity: O(n + m)
 export const commonLetters = (arr1: string[], arr2: string[]) => {
   const seen = new Set(arr1) // O(n)
