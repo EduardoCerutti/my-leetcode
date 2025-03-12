e6b79b1990dc91ec184b565a417159479605c092
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-03-11 22:28:33 -0300

    Remove brackets from tests title

--- src/leetCode/MergeSortedArrays.test.ts
+++ src/leetCode/MergeSortedArrays.test.ts
@@ -1,12 +1,12 @@
 import { mergeSortedArrays } from "./MergeSortedArrays"
 
-test("It should merge the arrays and keep it sorted returning [ 1, 3, 5, 6, 7, 8, 9]", () => {
+test("It should merge the arrays and keep it sorted returning 1, 3, 5, 6, 7, 8, 9", () => {
   expect(mergeSortedArrays([1, 5, 8, 9], [3, 6, 7])).toEqual([
     1, 3, 5, 6, 7, 8, 9,
   ])
 })
 
-test("It should merge the arrays and keep it sorted returning [2, 4, 5, 6, 7, 8, 9]", () => {
+test("It should merge the arrays and keep it sorted returning 2, 4, 5, 6, 7, 8, 9", () => {
   expect(mergeSortedArrays([2, 6, 8], [4, 5, 7, 9])).toEqual([
     2, 4, 5, 6, 7, 8, 9,
   ])
--- src/leetCode/SummedNumbers.test.ts
+++ src/leetCode/SummedNumbers.test.ts
@@ -1,9 +1,9 @@
 import { summedNumbers } from "./SummedNumbers"
 
-test("It should return true because 9 is a possible sum inside [5, 2, 6, 4]", () => {
+test("It should return true because 9 is a possible sum inside 5, 2, 6, 4", () => {
   expect(summedNumbers([5, 2, 6, 4], 9)).toBe(true)
 })
 
-test("It should return false because 11 isn't a possible sum inside [6, 1, 4, 3]", () => {
+test("It should return false because 11 isn't a possible sum inside 6, 1, 4, 3", () => {
   expect(summedNumbers([6, 1, 4, 3], 11)).toBe(false)
 })
