7f09d998a9146fa3630c67915084e4b5144feece
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-05-22 23:05:23 -0300

    Add more numbers to the test

--- src/theory/LinkedList.test.ts
+++ src/theory/LinkedList.test.ts
@@ -6,26 +6,27 @@ describe("LinkedList", () => {
   beforeEach(() => {
     linkedList = new LinkedList(5)
     linkedList.append(10)
+    linkedList.append(15)
   })
 
   test("It should preppend a new node to the beginning of the list", () => {
     linkedList.prepend(0)
     expect(linkedList.getHead()).toEqual({
       value: 0,
-      next: { value: 5, next: { value: 10, next: null } },
+      next: { value: 5, next: { value: 10, next: { value: 15, next: null } } },
     })
   })
 
   test("It should append a new node to the end of the list", () => {
-    linkedList.append(15)
-    expect(linkedList.getTail()).toEqual({ value: 15, next: null })
+    linkedList.append(20)
+    expect(linkedList.getTail()).toEqual({ value: 20, next: null })
   })
 
   test("It should insert a new node at the position 1 of the list", () => {
-    linkedList.insert(1, 7)
+    linkedList.insert(2, 12)
     expect(linkedList.getHead()).toEqual({
       value: 5,
-      next: { value: 7, next: { value: 10, next: null } },
+      next: { value: 10, next: { value: 12, next: { value: 15, next: null } } },
     })
   })
 })
