ea06505fae2c0fa897b01c129625a6a9166cc25c
Eduardo Cerutti <ceruttioliveira@hotmail.com>
2025-06-09 19:42:10 -0300

    Group tests under suites

--- src/leetCode/ReverseWord.test.ts
+++ src/leetCode/ReverseWord.test.ts
@@ -1,15 +1,17 @@
 import { reverseWord, reverseWord2, reverseWord3 } from "./ReverseWord"
 
-test('It should revert the word "awsome" and return', () => {
-  expect(reverseWord("awsome")).toBe("emoswa")
-})
+describe("ReverseWord", () => {
+  test('It should revert the word "awsome" and return', () => {
+    expect(reverseWord("awsome")).toBe("emoswa")
+  })
 
-test('It should revert the word "something" and return', () => {
-  expect(reverseWord2("something")).toBe("gnihtemos")
-})
+  test('It should revert the word "something" and return', () => {
+    expect(reverseWord2("something")).toBe("gnihtemos")
+  })
 
-test('It should revert the sentence "keep your hands to yourself" and return', () => {
-  expect(reverseWord3("keep your hands to yourself")).toBe(
-    "flesruoy ot sdnah ruoy peek"
-  )
+  test('It should revert the sentence "keep your hands to yourself" and return', () => {
+    expect(reverseWord3("keep your hands to yourself")).toBe(
+      "flesruoy ot sdnah ruoy peek"
+    )
+  })
 })
--- src/leetCode/SortedSummedNumbers.test.ts
+++ src/leetCode/SortedSummedNumbers.test.ts
@@ -1,9 +1,11 @@
 import { sortedSummedNumbers } from "./SortedSummedNumbers"
 
-test("It should return true because 8 is a possible sum inside 1, 2, 4, 4", () => {
-  expect(sortedSummedNumbers([1, 2, 4, 4], 8)).toBe(true)
-})
+describe("SortedSummedNumbers", () => {
+  test("It should return true because 8 is a possible sum inside 1, 2, 4, 4", () => {
+    expect(sortedSummedNumbers([1, 2, 4, 4], 8)).toBe(true)
+  })
 
-test("It should return false because 15 isn't a possible sum inside 1, 4, 5, 8", () => {
-  expect(sortedSummedNumbers([1, 4, 5, 8], 15)).toBe(false)
+  test("It should return false because 15 isn't a possible sum inside 1, 4, 5, 8", () => {
+    expect(sortedSummedNumbers([1, 4, 5, 8], 15)).toBe(false)
+  })
 })
